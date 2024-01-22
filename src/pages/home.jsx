eb910cd96b6745ba2db0d737ff35e5bafd1dbd0d
Arta Hendraa <artahenda@gmail.com>
2024-01-22 13:30:36 +0800

    add massage on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,13 @@
 const HomePage = () => {
   return (
-    <div>
-      <a href="/popular">popular</a>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-y-3">
+      <p>this page not available for now</p>
+      <a
+        href="/popular"
+        className=" bg-[#ece48b] text-black px-3 py-1 capitalize font-semibold rounded-md"
+      >
+        go to popular
+      </a>
     </div>
   );
 };
