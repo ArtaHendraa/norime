72ca8766ff17369d5ae25ad3420b3e5f7923b4f8
Arta Hendraa <artahenda@gmail.com>
2024-02-10 16:02:45 +0800

    update

--- src/components/Layouts/NavbarLayout.jsx
+++ src/components/Layouts/NavbarLayout.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const NavbarLayout = ({ children }) => {
   return (
-    <nav className="relative w-full h-14 md:h-[4.3rem] bg-transparent z-50">
+    <nav className="relative w-full h-16 md:h-[4.3rem] bg-transparent z-50">
       <div
         className="bg-[rgba(15,15,15,.9)] px-5 md:px-8 xl:px-0 text-white fixed left-0 right-0 backdrop-blur-sm"
         id="nav"
