629c023c6bf347365b4107ffae2d6f1b929504bb
Arta Hendraa <artahenda@gmail.com>
2024-02-14 13:28:41 +0800

    handle search

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -38,6 +38,10 @@ const Navbar = () => {
   const closeMenu = () => {
     setMenuOpen(false);
   };
+
+  const handleSearch = () => {
+    alert("Feature not available");
+  };
   return (
     <>
       <NavbarLayout>
@@ -59,6 +63,7 @@ const Navbar = () => {
         <CustomIcon
           classname="w-6 h-6 md:h-7 md:w-7 xl:hidden inline-block"
           icon="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          onclick={handleSearch}
         />
       </NavbarLayout>
       {isMenuOpen && (
