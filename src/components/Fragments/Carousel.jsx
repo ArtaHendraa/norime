c1a68eb1c13e19bd78fd9df93f7911f991e23362
Arta Hendraa <artahenda@gmail.com>
2024-02-22 23:40:22 +0800

    update carousel data

--- src/components/Fragments/Carousel.jsx
+++ src/components/Fragments/Carousel.jsx
@@ -13,18 +13,18 @@ const Carousel = () => {
 
   useEffect(() => {
     setLoading(true);
-    const animeIds = [38474, 32281, 55791, 33352, 54714, 49458, 48736]; // Replace with the desired array of IDs
+    const animeIds = [36792, 38474, 48316, 48548, 33352, 47917, 48736]; // Replace with the desired array of IDs
     getCarouselAnime(animeIds, (data) => {
       setCarousel(data);
       setLoading(false);
     });
   }, []);
 
-  const formatDate = (airedDate) => {
-    const parsedDate = new Date(airedDate);
-    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-    return parsedDate.toLocaleDateString("en-GB", options);
-  };
+  // const formatDate = (airedDate) => {
+  //   const parsedDate = new Date(airedDate);
+  //   const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  //   return parsedDate.toLocaleDateString("en-GB", options);
+  // };
 
   return (
     <>
@@ -68,9 +68,27 @@ const Carousel = () => {
                         <Hading classname="hidden md:block text-sm text-[#ece48b] whitespace-nowrap overflow-hidden text-ellipsis w-3/5">
                           {carouselItem.title_japanese}
                         </Hading>
-                        <p className="text-sm font-light">
-                          {formatDate(carouselItem.aired.from)}
-                        </p>
+                        <div className="flex items-center gap-1 mt-1">
+                          {/* {formatDate(carouselItem.aired.from)} */}
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                            />
+                          </svg>
+
+                          <p className="text-sm font-light ">
+                            {carouselItem.score}
+                          </p>
+                        </div>
                       </div>
                     </div>
 
