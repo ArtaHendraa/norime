6bf7aae7f3d88d8e11538687cc685b2e0a4e9f41
Arta Hendraa <artahenda@gmail.com>
2024-02-25 13:35:57 +0800

    update carousel design

--- src/components/Fragments/Carousel.jsx
+++ src/components/Fragments/Carousel.jsx
@@ -49,22 +49,25 @@ const Carousel = () => {
               <section>
                 <a href={`anime/${carouselItem.mal_id}`}>
                   <main className="flex justify-between items-end bg-[#141518] relative">
-                    <div className="absolute w-full h-full xl:flex xl:items-end xl:carousel-shadow-xl carousel-shadow-sm">
+                    <div className="absolute w-full h-full xl:flex xl:items-end carousel-shadow-sm md:carousel-shadow-md xl:carousel-shadow-xl">
                       <img
                         className="hidden object-contain object-left w-auto m-4 rounded-md xl:block"
                         src={carouselItem.images.webp.image_url}
                         alt={carouselItem.title}
                       />
                       <div className="flex flex-col items-start justify-end h-full px-3 pb-4">
-                        <Banner classname="bg-[#ece48b] text-[#141518] text-[9px]">
-                          recommend
-                        </Banner>
-                        <Hading classname="w-3/4 overflow-hidden text-xl font-bold md:w-full text-neutral-100 xl:text-3xl whitespace-nowrap text-ellipsis">
-                          {carouselItem.title}
-                        </Hading>
-                        <Hading classname="hidden md:block text-sm text-[#ece48b] whitespace-nowrap overflow-hidden text-ellipsis">
-                          {carouselItem.title_japanese}
-                        </Hading>
+                        <div className="w-full">
+                          <Banner classname="bg-[#ece48b] text-[#141518] text-[0.563rem]">
+                            recommend
+                          </Banner>
+                          <Hading classname="w-3/4 overflow-hidden text-xl font-bold w-3/ md:w-full text-neutral-100 xl:text-3xl whitespace-nowrap text-ellipsis">
+                            {carouselItem.title}
+                          </Hading>
+                          <Hading classname="hidden md:block text-sm text-[#ece48b] whitespace-nowrap overflow-hidden text-ellipsis">
+                            {carouselItem.title_japanese}
+                          </Hading>
+                        </div>
+
                         <div className="flex items-center gap-2 text-xs md:text-base">
                           <div className="flex items-center gap-1">
                             <svg
@@ -84,7 +87,7 @@ const Carousel = () => {
 
                             <p>{carouselItem.aired.prop.from.year}</p>
                           </div>
-                          |
+                          <span>|</span>
                           <div className="flex items-center gap-1">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -104,11 +107,11 @@ const Carousel = () => {
                           </div>
                         </div>
 
-                        <div className="flex items-center gap-2 mt-1">
+                        <div className="flex items-center gap-2 mt-1 mb-1">
                           {carouselItem.genres.map((genre, index) => (
                             <Banner
                               key={index + 1}
-                              classname="bg-[#444547] text-[9px] text-neutral-100"
+                              classname="bg-[#444547] text-[0.563rem] text-neutral-100"
                             >
                               {genre.name}
                             </Banner>
@@ -119,7 +122,7 @@ const Carousel = () => {
 
                     <div className="w-full">
                       <img
-                        className="w-full md:h-[500px] object-contain object-right"
+                        className="w-full md:h-[31.25rem] sm:object-contain md:object-cover xl:object-contain object-right"
                         loading="lazy"
                         src={carouselItem.trailer.images.maximum_image_url}
                         alt={carouselItem.title}
