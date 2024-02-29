a18f3d86916f4549f0ccab822db0f2d1738df755
Arta Hendraa <artahenda@gmail.com>
2024-03-01 00:11:38 +0800

    update detailAnime.jsx

--- src/pages/detailAnime.jsx
+++ src/pages/detailAnime.jsx
@@ -76,7 +76,8 @@ const DetailAnime = () => {
                   text={detail.status}
                 />
               </div>
-              <div className="flex items-center gap-2 mt-1 mb-3">
+
+              <div className="flex items-center gap-2 mt-1">
                 {detail.genres.map((genre, index) => (
                   <Banner
                     key={index + 1}
@@ -86,6 +87,22 @@ const DetailAnime = () => {
                   </Banner>
                 ))}
               </div>
+
+              <div className="flex items-center justify-center gap-5 my-3">
+                <Button classname="bg-[#ece48b] text-black w-full flex items-center justify-center capitalize font-semibold py-2 rounded-full">
+                  <MoreInfo
+                    text="bookmark"
+                    icon="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                  />
+                </Button>
+                <Button classname="bg-[#ece48b] text-black w-full flex items-center justify-center capitalize font-semibold py-2 rounded-full">
+                  <MoreInfo
+                    text={detail.broadcast.day}
+                    icon="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                  />
+                </Button>
+              </div>
+
               <Synopsis detail={detail} />
               <Episode episodes={episodes} />
             </div>
@@ -165,9 +182,13 @@ const Episode = (props) => {
 };
 
 const MoreInfo = (props) => {
-  const { icon, text } = props;
+  const {
+    icon,
+    text,
+    classname = "flex items-center justify-center gap-1",
+  } = props;
   return (
-    <div className="flex items-center gap-1">
+    <div className={classname}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
