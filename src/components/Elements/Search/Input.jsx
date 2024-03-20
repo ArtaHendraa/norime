5c86a4d932275e23e04f5fa981b43fa38bad9d75
Arta Hendrawan <artahendraa.dev@gmail.com>
2024-03-20 23:51:00 +0800

    update search

--- src/components/Elements/Search/Input.jsx
+++ src/components/Elements/Search/Input.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 const Input = (props) => {
-  const { type, placeholder, classname } = props;
+  const { type, placeholder, classname, onChange } = props;
   return (
     <input
       type={type}
       placeholder={placeholder}
       className={`bg-[rgba(255,255,255,.1)] py-2 px-5 ${classname} focus:outline-none rounded-l-md`}
+      required
+      onChange={onChange}
     />
   );
 };
